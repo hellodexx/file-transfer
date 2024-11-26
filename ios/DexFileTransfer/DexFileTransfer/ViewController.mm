7505725408f40ab44412f5f6585041ae383d1efa
Dexter <dexalberto88@gmai.com>
2024-11-26 13:53:19 +0800

    Implement initial photo picker

--- ios/DexFileTransfer/DexFileTransfer/ViewController.mm
+++ ios/DexFileTransfer/DexFileTransfer/ViewController.mm
@@ -6,15 +6,21 @@
 //
 
 #import "ViewController.h"
-#include "DexFtServer.h"
+#import <UIKit/UIKit.h>
 #import <Photos/Photos.h>
+#import <PhotosUI/PhotosUI.h>
+#import <UniformTypeIdentifiers/UniformTypeIdentifiers.h> // Correct framework for UTType
+#import <AVFoundation/AVFoundation.h>
+
+#include "DexFtServer.h"
 // Libraries to get local private address
 #import <ifaddrs.h>
 #import <arpa/inet.h>
 
-@interface ViewController ()
+@interface ViewController () <PHPickerViewControllerDelegate>
 @property (weak, nonatomic) IBOutlet UILabel *ipLabel;
 @property (weak, nonatomic) IBOutlet UISwitch *startSwitch;
+@property (strong, nonatomic) NSMutableDictionary *mediaDictionary;
 @end
 
 @implementation ViewController
@@ -47,7 +53,13 @@ Dex::FileTransferServer ft;
     NSString *privateIPAddress = [self getPrivateIPAddress];
     NSLog(@"Private IP Address: %@", privateIPAddress);
     _ipLabel.text = privateIPAddress;
+    
+    // Initialize the dictionary
+    self.mediaDictionary = [NSMutableDictionary dictionary];
+}
 
+- (IBAction)sendPhotosButtonPressed:(id)sender {
+    [self pickMediaFiles];
 }
 
 - (IBAction)startSwitchChanged:(id)sender {
@@ -101,6 +113,72 @@ Dex::FileTransferServer ft;
     freeifaddrs(interfaces);
 
     return address;
+}
+
+- (void)pickMediaFiles {
+    // PHPickerViewController for iOS 14+
+    PHPickerConfiguration *config = [[PHPickerConfiguration alloc] init];
+    config.selectionLimit = 0;  // Set to 0 for unlimited selection
+    config.filter = [PHPickerFilter imagesFilter];  // Only images, if required
+    
+    PHPickerViewController *picker = [[PHPickerViewController alloc] initWithConfiguration:config];
+    picker.delegate = self;  // Set delegate to self
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+#pragma mark - PHPickerViewController Delegate Methods
+
+// Delegate method for PHPickerViewController (iOS 14+)
+- (void)picker:(PHPickerViewController *)picker didFinishPicking:(NSArray<PHPickerResult *> *)results {
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    
+    // Ensure that results are not empty
+    if (results.count == 0) {
+        NSLog(@"No files selected.");
+        return;
+    }
+    
+    // Print each file selected
+    for (PHPickerResult *result in results) {
+        // Attempt to load the object as a UIImage for images or an AVURLAsset for videos
+        [result.itemProvider loadObjectOfClass:[UIImage class] completionHandler:^(id  _Nullable object, NSError * _Nullable error) {
+            if (object && [object isKindOfClass:[UIImage class]]) {
+                // Handle image
+                NSLog(@"Selected image: %@", object);
+                
+                // Format the date (you can do this for the image if required, here is just an example):
+                NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+                [formatter setDateFormat:@"yyyyMMdd_HHmmss"];
+                NSString *formattedDate = [formatter stringFromDate:[NSDate date]];  // You may need to associate with actual asset date
+                
+                // Save the image in dictionary with a timestamp as the key (use .jpg extension)
+                NSString *key = [formattedDate stringByAppendingString:@".jpg"];
+                self.mediaDictionary[key] = object;
+                
+                // Print asset details
+                NSLog(@"Image file saved with key: %@", key);
+            } else if (object && [object isKindOfClass:[AVURLAsset class]]) {
+                // Handle video
+                AVURLAsset *videoAsset = (AVURLAsset *)object;
+                
+                NSLog(@"Selected video URL: %@", videoAsset.URL);
+                
+                // Similarly format date for video
+                NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+                [formatter setDateFormat:@"yyyyMMdd_HHmmss"];
+                NSString *formattedDate = [formatter stringFromDate:[NSDate date]];  // Adjust accordingly
+                
+                // Save video in dictionary with timestamp as the key (use .mp4 extension)
+                NSString *key = [formattedDate stringByAppendingString:@".mp4"];
+                self.mediaDictionary[key] = videoAsset;
+                
+                // Print asset details
+                NSLog(@"Video file saved with key: %@", key);
+            } else {
+                NSLog(@"Error loading asset: %@", error.localizedDescription);
+            }
+        }];
+    }
 }
 
 @end
